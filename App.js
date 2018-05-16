d1bb2c0765faffbce285c7df962450d6749e20d1
Stephen Kingsley <stephenkingsley@Stephens-MacBook-Pro.local>
2018-05-15 23:11:16 -0500

    Finished Project App

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, Button, ScrollView, TouchableOpacity } from 'react-native'
 import moment from 'moment'
 
 function Timer({ interval, style }) {
+	const pad = (n) => n < 10 ? '0' + n : n
 	const duration = moment.duration(interval)
 	const centiseconds = Math.floor(duration.milliseconds() / 10)
 	return (
-		<Text style={ style }>
-		{ duration.minutes() }:{duration.seconds()},{centiseconds}
-	      </Text>
+	<View style={styles.timerContainer}>
+		<Text style={ style }>{pad(duration.minutes())}:</Text>
+		<Text style={ style }>{pad(duration.seconds())},</Text>
+		<Text style={ style }>{pad(centiseconds)}</Text>
+	</View>
 			)
-} //left off at 28:30
+}
 
 function ButtonsRow({ children }) {
 	return(
@@ -18,13 +21,17 @@ function ButtonsRow({ children }) {
 	)
 }
 
-function RoundButton({ title, color, background }) {
+function RoundButton({ title, color, background, onPress, disabled }) {
 	return (
-		<View style={[ styles.button,{ backgroundColor: background }]}>
+		<TouchableOpacity
+      onPress={() => !disabled && onPress()}
+			style={[ styles.button,{ backgroundColor: background }]}
+      activeOpactiy={disabled ? 1.0 : 0.7}
+			>
 			<View style={styles.buttonBorder}>
 			 <Text style={[ styles.buttonTitle,{ color }]}>{title}</Text>
 			</View>
-		</View>
+		</TouchableOpacity>
 	)
 }
 
@@ -37,12 +44,12 @@ function Lap({ number, interval, fastest, slowest }) {
 	return(
 	<View style={styles.lap}>
 		<Text style={lapStyle}>Lap {number}</Text>
-		<Timer style={lapStyle} interval={interval}/>
+		<Timer style={[lapStyle, styles.lapTimer]} interval={interval}/>
 	</View>
  )
 }
 
-function LapsTable({ laps }) {
+function LapsTable({ laps, timer }) {
 	const finishedLaps = laps.slice(1)
 	let min = Number.MAX_SAFE_INTEGER
 	let max = Number.MIN_SAFE_INTEGER
@@ -58,7 +65,7 @@ function LapsTable({ laps }) {
 				<Lap
 					number={ laps.length - index}
 					key={ laps.length - index }
-					interval={lap}
+					interval={index == 0 ? timer + lap : lap}
 					fastest={lap == min}
 					slowest={lap == max}
 				/>
@@ -67,7 +74,7 @@ function LapsTable({ laps }) {
 	)
 }
 
-class App extends Component {
+default export class App extends Component {
 
   constructor(props) {
   	super(props)
@@ -78,6 +85,10 @@ class App extends Component {
   		}
   		this.handleButtonPressed = this.handleButtonPressed.bind(this)
   }
+
+  componentWillUnmount() {
+		clearInterval(this.timer)
+	}
 
   componentDidMount() {
   	 this.timer = setInterval(() => {
@@ -93,18 +104,117 @@ class App extends Component {
   this.setState({ Test: this.state.Test + 1})
 }
 
+start = () => {
+  const now = new Date().getTime()
+	this.setState({
+		start: now,
+		now,
+		laps: [0],
+	})
+	this.timer = setInterval(() => {
+		this.setState({ now: new Date().getTime()})
+	} , 100)
+}
+
+lap = () => {
+	const timestamp = new Date().getTime()
+	const { laps, now, start } = this.state
+	const [firstlap, ...other] = laps
+	this.setState({
+		laps: [0, firstlap + now - start, ...other],
+		start: timestamp,
+		now: timestamp,
+	})
+}
+
+stop = () => {
+	clearInterval(this.timer)
+	const { laps, now, start } = this.state
+	const [firstlap, ...other] = laps
+	this.setState({
+		laps: [firstlap + now - start, ...other],
+		start: 0,
+		now: 0,
+	})
+}
+
+reset = () => {
+	this.setState({
+		laps: [],
+		start: 0,
+		now: 0,
+	})
+}
+
+resume = () => {
+	const now = new Date().getTime()
+	this.setState({
+		start: now,
+		now: now,
+	})
+	this.timer = setInterval(() => {
+		this.setState({ now: new Date().getTime()})
+	} , 100)
+}
 
   render() {
 		const { now, start, laps } = this.state
-		const timer = now - start //left off on 37:34
+		const timer = now - start
     return (
       <View style={ styles.container }>
-       <Timer interval={ timer } style={styles.timer}/>
-			 <ButtonsRow>
-			   <RoundButton title='Reset' color='#FFFFFF' background='#3D3D3D' />
-			   <RoundButton title='Start' color='#50D167' background='#1B361F' />
+       <Timer
+				 interval={laps.reduce((total, curr) => total + curr, 0) + timer }
+				 style={styles.timer}
+			 />
+			 {laps.length == 0 && (
+				 <ButtonsRow>
+				   <RoundButton
+						 title='Lap'
+						 color='#8B8B90'
+						 background='#151515'
+						 disabled
+					 />
+				   <RoundButton
+						 title='Start'
+						 color='#50D167'
+						 background='#1B361F'
+						 onPress={this.start}
+					 />
+				 </ButtonsRow>
+			 )}
+			 {start > 0 && (
+				 <ButtonsRow>
+					<RoundButton
+						title='Lap'
+						color='#FFFFFF'
+						background='#3D3D3D'
+						onPress={this.lap}
+					/>
+					<RoundButton
+						title='Stop'
+						color='#E33925'
+						background='#3C1715'
+						onPress={this.stop}
+					/>
+				</ButtonsRow>
+			)}
+			{laps.length > 0 && start == 0 && (
+				<ButtonsRow>
+				 <RoundButton
+					 title='Reset'
+					 color='#FFFFFF'
+					 background='#3D3D3D'
+					 onPress={this.reset}
+				 />
+				 <RoundButton
+					 title='Start'
+					 color='#50D167'
+					 background='#1B361F'
+					 onPress={this.resume}
+				 />
 			 </ButtonsRow>
-			 <LapsTable laps={laps} />
+		 )}
+			 <LapsTable laps={laps} timer={timer}/>
       </View>
       )
   }
@@ -121,7 +231,8 @@ const styles = StyleSheet.create({
 	timer: {
 		color: '#FFFFFF',
 		fontSize: 76,
-		fontWeight: '200'
+		fontWeight: '200',
+		width: 110,//left off 46:36
 	},
 	button: {
 		width: 80,
@@ -137,7 +248,7 @@ const styles = StyleSheet.create({
 		width: 76,
 		height: 76,
 		borderRadius: 38,
-		borderWidth: 2,
+		borderWidth: 1,
     justifyContent: 'center',
 		alignItems: 'center',
 	},
@@ -151,6 +262,9 @@ const styles = StyleSheet.create({
 	lapText: {
 		color: '#FFFFFF',
 		fontSize: 18,
+	},
+	lapTimer: {
+		width: 30,
 	},
 	lap: {
 		flexDirection: 'row',
@@ -168,6 +282,9 @@ const styles = StyleSheet.create({
 	slowest: {
 		color: '#CC3531'
 	},
+	timerContainer: {
+		flexDirection: 'row',
+	}
 })
 
 export default App
